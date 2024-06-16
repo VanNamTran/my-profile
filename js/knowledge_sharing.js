f52825ad244d9dfa202772cff19ae0f4e62c90c5
Admin <tranvannamntt@gmail.com>
2024-06-16 08:58:13 +0700

    modified:   Index.html 	modified:   blog.html 	modified:   css/format.css 	modified:   css/styles.css 	modified:   data/knowledge_sharing.json 	renamed:    images/reactnantive.jpeg -> images/reactnative.jpeg 	new file:   js/knowledge_sharing.js 	modified:   knowledge_sharing.html

---
+++ js/knowledge_sharing.js
@@ -0,0 +1,24 @@
+document.addEventListener('DOMContentLoaded', function () {
+      fetch('./data/knowledge_sharing.json')
+          .then(response => response.json())
+          .then(data => {
+              const container = document.getElementById('data-container');
+              const template = document.getElementById('data-template');
+
+              data.forEach(item => {
+                  const clone = document.importNode(template.content, true);
+
+                  // Set background image
+                  clone.querySelector('.background-image img').src = `images/${item.image}`;
+                  // Set name
+                  clone.querySelector('.topic-name').textContent = item.name;
+                  // Set fields
+                  clone.querySelector('.fields').textContent = item.field.join(', ');
+
+                  container.appendChild(clone);
+              });
+          })
+          .catch(error => console.error('Error fetching data:', error));
+  });
+    
+    
