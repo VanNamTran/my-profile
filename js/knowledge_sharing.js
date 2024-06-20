3a3e976f3b14be366731eb4c1a0254275b523e45
Admin <tranvannamntt@gmail.com>
2024-06-20 19:35:33 +0700

    modified:   Index.html 	modified:   css/styles.css 	new file:   images/menu.png 	modified:   js/knowledge_sharing.js 	modified:   knowledge_sharing.html

--- js/knowledge_sharing.js
+++ js/knowledge_sharing.js
@@ -21,19 +21,4 @@ document.addEventListener('DOMContentLoaded', function () {
         .catch(error => console.error('Error fetching data:', error));
 });
 
-// scroll to top button
-var scrollToTopBtn = document.getElementById("scrollToTopBtn");
-window.onscroll = function () { scrollFunction() };
 
-function scrollFunction() {
-    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-        scrollToTopBtn.style.display = "block";
-    } else {
-        scrollToTopBtn.style.display = "none";
-    }
-}
-
-scrollToTopBtn.onclick = function () {
-    document.body.scrollTop = 0;
-    document.documentElement.scrollTop = 0;
-}
